b73b203b66e48205f6b417dec4169fa4c17f141d
efekocmn4-hash <efekocmn4@gmail.com>
2026-08-07 20:25:59 +0300

    main.py

--- main.py
+++ main.py
@@ -168,5 +168,7 @@ import os
 
 # ... bot kodunun geri kalanı ...
 
-token = os.environ['DISCORD_TOKEN']
-bot.run("MTUzNTMxNDQzMzE1OTQ2MzAzMw.GJJS2J.8JVYxn5G73fabiwMq3_PivWW0n64tnBQYNGdJE")
+import os
+
+bot.run(os.environ['DISCORD_TOKEN'])
+
